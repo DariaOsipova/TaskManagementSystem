766b6296000c5c6e1c427f22977f659530992437
DariaOsipova <darya.osipova.2005@list.ru>
2026-06-23 18:17:13 +0700

    Update DesignTimeDbContextFactory.cs

--- Infrastructure/DesignTimeDbContextFactory.cs
+++ Infrastructure/DesignTimeDbContextFactory.cs
@@ -1,21 +1,24 @@
-
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Infrastructure.Data;
 
 namespace Infrastructure
 {
-    //// Реализует интерфейс IDesignTimeDbContextFactory для EF Core инструментов
+    // Реализует интерфейс IDesignTimeDbContextFactory для EF Core инструментов
     public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     {
-        //// Метод создания экземпляра AppDbContext для миграций EF Core
+        // Метод создания экземпляра AppDbContext для миграций EF Core
         public AppDbContext CreateDbContext(string[] args)
         {
-            // // Создаем построитель опций для контекста базы данных
+            // Создаем построитель опций для контекста базы данных
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
 
-       
-            var connectionString = "Host=localhost;Port=5432;Database=RealEstateDB;Username=postgres;Password=123456;";
+            // ИСПРАВЛЕННАЯ СТРОКА ПОДКЛЮЧЕНИЯ:
+            // - Порт: 5432 (правильный, был 5433)
+            // - База: PropertyManagerDb (правильная, была RealEstateDB)
+            // - Пароль: 123456 (правильный, был password)
+            var connectionString = "Host=localhost;Port=5432;Database=PropertyManagerDb;Username=postgres;Password=123456;";
+            
             optionsBuilder.UseNpgsql(connectionString);
 
             return new AppDbContext(optionsBuilder.Options);
